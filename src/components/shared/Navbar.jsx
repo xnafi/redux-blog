02769d1fd4bfa9a518af2f2ad578a564e9c8c1e5
Forhad Khan <fk.nafi11@gmail.com>
2025-08-16 16:06:18 +0600

    side bar created

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,17 +2,17 @@ import logo from "../../assets/images/LWSBlog.svg";
 
 export default function Navbar() {
   return (
-    <nav class="py-4 border-b">
-      <div class="navbar-container">
-        <div class="logo">
+    <nav className="py-4 border-b">
+      <div className="navbar-container">
+        <div className="logo">
           <a href="index.html">
             <img src={logo} alt="search" />
           </a>
         </div>
         {/* <!-- auth buttons , This will nonfunctional, just for nice looking --> */}
-        <div class="auth-buttons">
-          <button class="btn btn-primary">sign in</button>
-          <button class="btn btn-outline">sign up</button>
+        <div className="auth-buttons">
+          <button className="btn btn-primary">sign in</button>
+          <button className="btn btn-outline">sign up</button>
         </div>
       </div>
     </nav>
