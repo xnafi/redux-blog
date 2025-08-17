661478fe38cc4a0634c4a310095ddf5e8307d4a0
Forhad Khan <fk.nafi11@gmail.com>
2025-08-17 16:03:50 +0600

    blog details ui created

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+
+import BlogDetailCard from './BlogDetailCard';
+import BlogRelated from './BlogRelated';
 
 export default function BlogDetails() {
   return (
-    <div>B</div>
-  )
+    <section className="post-page-container">
+      <BlogDetailCard/>
+      {/* <!-- detailed post ends --> */}
+      {/* <!-- related posts --> */}
+      <BlogRelated />
+      {/* <!-- related posts ends --> */}
+    </section>
+  );
 }
