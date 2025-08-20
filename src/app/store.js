e25dede906f2c85048276affaa36b29b25381522
Forhad Khan <fk.nafi11@gmail.com>
2025-08-20 14:17:35 +0600

    added in the store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import blogsReducer from "../features/AllBlogsSlice/AllBlogsSlice";
 import relatedBlogReducer from "../features/RelatedBlogs/RelatedBlogsSlice";
 import blogReducer from "../features/SingleBlog/SingleBlogSlice";
+import updateStatusReducer from "../features/UpdateStatusAndLike/UpdateStatusAndLikeSlice";
 
 export const store = configureStore({
   reducer: {
     blogs: blogsReducer,
     blog: blogReducer,
     relatedBlogs: relatedBlogReducer,
+    updateStatus: updateStatusReducer,
   },
 });
